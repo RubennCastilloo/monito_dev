48656a0ab3ad68d410f2bb9605c7f05e4ddb15f5
Ruben Castillo <ruben._12@live.com>
2022-09-08 20:24:03 -0600

    Se cambia barra de navegación

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import Nav from './components/Nav.vue'
 </script>
 
 <template>
@@ -8,13 +7,9 @@ import TheWelcome from './components/TheWelcome.vue'
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <Nav />
     </div>
   </header>
-
-  <main>
-    <TheWelcome />
-  </main>
 </template>
 
 <style scoped>
